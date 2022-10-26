c810d61fe4b6cd41ae91ee4dfb546662a057723a
marcomammi02 <marcomammi2002@gmail.com>
2022-10-26 18:34:32 +0200

    Adding basic logic

---
+++ script.js
@@ -0,0 +1,41 @@
+const arrImages = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const eleSliderContainer = document.querySelector('.slider-container');
+
+for (i = 0; i < arrImages.length; i++) {
+    const eleImg = document.createElement('img');
+    eleImg.src = arrImages[i];
+    
+    if (i == 0) {
+        eleImg.classList.add('active');
+    }
+
+    eleSliderContainer.append(eleImg);
+}
+
+const eleBtnPrev = document.querySelector('.btn-prev');
+const eleBtnNext = document.querySelector('.btn-next');
+const eleImgList = document.querySelectorAll('img');
+
+let activeIndex = 0
+
+eleBtnNext.addEventListener('click', function() {
+    if (activeIndex < eleImgList.length - 1) {
+        eleImgList[activeIndex].classList.remove('active');
+        activeIndex++;
+        eleImgList[activeIndex].classList.add('active');
+    }
+})
+eleBtnPrev.addEventListener('click', function() {
+    if (activeIndex > 0) {
+        eleImgList[activeIndex].classList.remove('active');
+        activeIndex--;
+        eleImgList[activeIndex].classList.add('active');
+    }
+})
