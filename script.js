8c83622fc31eb0ada72bb6b210f657b8e0a564f8
marcomammi02 <marcomammi2002@gmail.com>
2022-11-08 18:41:37 +0100

    Final version

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ const arrImages = [
 ];
 
 const eleSliderContainer = document.querySelector('.slider-container');
+const eleSmallImgContainer = document.querySelector('.small-img-container');
 
-for (i = 0; i < arrImages.length; i++) {
+for (let i = 0; i < arrImages.length; i++) {
     const eleImg = document.createElement('img');
     eleImg.src = arrImages[i];
-    
+    eleImg.classList.add('slider-img');
+    eleSmallImgContainer.innerHTML += `<img class="small-img" src="${arrImages[i]}">`;
+    const eleSmallImg = document.querySelector('.small-img');
+
     if (i == 0) {
         eleImg.classList.add('active');
+        eleSmallImg.classList.add('active-small');
     }
 
     eleSliderContainer.append(eleImg);
@@ -21,14 +26,16 @@ for (i = 0; i < arrImages.length; i++) {
 
 const eleBtnPrev = document.querySelector('.btn-prev');
 const eleBtnNext = document.querySelector('.btn-next');
-const eleImgList = document.querySelectorAll('img');
+const eleImgList = document.querySelectorAll('.slider-img');
+const eleSmallImgList = document.querySelectorAll('.small-img')
 
 let activeIndex = 0
 
 eleBtnNext.addEventListener('click', function() {
     if (activeIndex < eleImgList.length) {
         eleImgList[activeIndex].classList.remove('active');
-        
+        eleSmallImgList[activeIndex].classList.remove('active-small');
+
         activeIndex++;
 
         if (activeIndex == eleImgList.length) {
@@ -36,11 +43,14 @@ eleBtnNext.addEventListener('click', function() {
         } 
 
         eleImgList[activeIndex].classList.add('active');
+        eleSmallImgList[activeIndex].classList.add('active-small');
+
     }
 })
 eleBtnPrev.addEventListener('click', function() {
     if (activeIndex >= 0) {
         eleImgList[activeIndex].classList.remove('active');
+        eleSmallImgList[activeIndex].classList.remove('active-small');
 
         if (activeIndex == 0) {
             activeIndex = eleImgList.length - 1;
@@ -49,5 +59,7 @@ eleBtnPrev.addEventListener('click', function() {
         }
 
         eleImgList[activeIndex].classList.add('active');
+        eleSmallImgList[activeIndex].classList.add('active-small');
+
     }
 })
